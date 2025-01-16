6d12a2f536b0998c9d62bf4874869aec7afed3d5
raflorenz <rjlorenz931@gmail.com>
2025-01-16 23:59:03 +0800

    add authentication

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,61 @@
-import { pgTable, text, integer, doublePrecision } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  text,
+  integer,
+  doublePrecision,
+  timestamp,
+  boolean,
+} from "drizzle-orm/pg-core";
+
+export const user = pgTable("user", {
+  id: text().primaryKey(),
+  name: text().notNull(),
+  email: text().notNull().unique(),
+  emailVerified: boolean().notNull(),
+  image: text(),
+  createdAt: timestamp().notNull(),
+  updatedAt: timestamp().notNull(),
+});
+
+export const session = pgTable("session", {
+  id: text().primaryKey(),
+  expiresAt: timestamp().notNull(),
+  token: text().notNull().unique(),
+  createdAt: timestamp().notNull(),
+  updatedAt: timestamp().notNull(),
+  ipAddress: text(),
+  userAgent: text(),
+  userId: text()
+    .notNull()
+    .references(() => user.id),
+});
+
+export const account = pgTable("account", {
+  id: text().primaryKey(),
+  accountId: text().notNull(),
+  providerId: text().notNull(),
+  userId: text()
+    .notNull()
+    .references(() => user.id),
+  accessToken: text(),
+  refreshToken: text(),
+  idToken: text(),
+  accessTokenExpiresAt: timestamp(),
+  refreshTokenExpiresAt: timestamp(),
+  scope: text(),
+  password: text(),
+  createdAt: timestamp().notNull(),
+  updatedAt: timestamp().notNull(),
+});
+
+export const verification = pgTable("verification", {
+  id: text().primaryKey(),
+  identifier: text().notNull(),
+  value: text().notNull(),
+  expiresAt: timestamp().notNull(),
+  createdAt: timestamp(),
+  updatedAt: timestamp(),
+});
 
 export const mediaTable = pgTable("media", {
   id: integer().primaryKey().notNull(),
@@ -12,4 +69,7 @@ export const mediaTable = pgTable("media", {
   number_of_episodes: integer(),
   runtime: integer(),
   status: text(),
+  userId: text()
+    .notNull()
+    .references(() => user.id),
 });
