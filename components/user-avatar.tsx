2444decb40e77711d3d49f5314701b49ce89ab22
raflorenz <rjlorenz931@gmail.com>
2025-01-27 23:45:16 +0800

    add user avatar and dropdown menu

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -1,13 +1,41 @@
 "use client";
 
 import { authClient } from "@/lib/auth-client";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 export default function UserAvatar() {
   const { data } = authClient.useSession();
 
   if (data) {
     return (
-      <button onClick={async () => await authClient.signOut()}>Sign Out</button>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="outline-none">
+          <Avatar>
+            <AvatarImage src={data.user.image!} alt="avatar image" />
+            <AvatarFallback>UA</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>
+            {data.user.name} <br />
+            {data.user.email}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <button onClick={async () => await authClient.signOut()}>
+              Sign Out
+            </button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     );
   }
 }
